140360ff59a80bfb51fe4c32e1881161bd49d925
Samia Naz <nsamia312@gmail.com>
2026-05-27 23:43:49 +0500

    Added 1D array program with traversal

---
+++ OneDArray.java
@@ -0,0 +1,13 @@
+public class OneDArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("1D Array Elements:");
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
