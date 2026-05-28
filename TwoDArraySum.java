b785598f92aabf6b5f128e2abb7e4fce76564757
Samia Naz <nsamia312@gmail.com>
2026-05-28 23:48:09 +0500

    Added 2D array sum program using nested loops

---
+++ TwoDArraySum.java
@@ -0,0 +1,20 @@
+public class TwoDArraySum {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
